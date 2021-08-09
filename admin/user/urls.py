eb4d521aadcb83208f572f38a1941b5cbca0f20b
hellmendos <poznkirill3@gmail.com>
2021-08-09 23:14:22 +0300

    Beta 1.0

--- admin/user/urls.py
+++ admin/user/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
         UserCheck.as_view()),
     url('user/list/',
         UserList.as_view()),
+    url('user/add/points/(?P<token>[0-9а-яА-Яa-zA-Z]+)/(?P<points>[0-9]+)/?$',
+        UserAddPoints.as_view()),
     url('user/stat/top/(?P<token>[0-9а-яА-Яa-zA-Z]+)/(?P<top>[0-1]+)/(?P<ys>[0-1]+)/(?P<tooday>[0-1]+)/(?P<qest>[0-1]+)/?$',
         UserStatTop.as_view()),
     url('action/create/(?P<token>[0-9а-яА-Яa-zA-Z]+)/(?P<code>[0-1]+)/?$',
